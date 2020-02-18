3647bd9a823a5d81f071d900471156f2e6a6ca20
Johan <jerosario@itla.edu.do>
2020-02-18 17:20:48 -0400

    Este es el commit inicial para subir el proyecto con su primera modificacion: La adicion de un controlador Ejemplo y una vista

---
+++ Ejemplo/Ejemplo/Controllers/EjemploController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo.Controllers
+{
+    public class EjemploController : Controller
+    {
+        // GET: Ejemplo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Vista()
+        {
+            return View();
+        }
+    }
+}
